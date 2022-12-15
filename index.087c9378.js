e1b2e315bd71b3939bf2326b20ef0205567ad2dd
ultrasoundcode <0x.ultrasound@gmail.com>
2022-12-15 02:46:56 +0000

    Deploying to gh-pages from @ ultrasoundcode/goit-ice-cream-project@7b1c52a96c16d8ae3481cace1c636db7fd9dd1ee 🚀

---
+++ index.087c9378.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".mySwiper",{spaceBetween:30,pagination:{el:".swiper-pagination",clickable:!0}});
+//# sourceMappingURL=index.087c9378.js.map
--- index.42e7b475.js
+++
@@ -1,2 +0,0 @@
-var swiper=new Swiper(".mySwiper",{pagination:{el:".swiper-pagination"}});
-//# sourceMappingURL=index.42e7b475.js.map
