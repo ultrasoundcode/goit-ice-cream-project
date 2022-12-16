606bff5d3edbe670054d0b428e7bd6e82d697059
Maracasimova <102946830+Maracasimova@users.noreply.github.com>
2022-12-16 20:31:16 +0000

    Deploying to gh-pages from @ ultrasoundcode/goit-ice-cream-project@94db330fe45da668338a83455deb9d96cbb0c7d6 🚀

--- index.314caaa6.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e={openMenuBtn:document.querySelector("[data-menu-open]"),closeMenuBtn:document.querySelector("[data-menu-close]"),menu:document.querySelector("[data-menu]")};function n(){e.menu.classList.toggle("is-open")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)})();
-//# sourceMappingURL=index.314caaa6.js.map
